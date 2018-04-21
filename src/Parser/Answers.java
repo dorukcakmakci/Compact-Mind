fe75dfa4d8a0adbb6e2948894e6237c4c1361bd8
bugraaydin <bugraaydin.cs@gmail.com>
2018-04-21 08:28:17 +0300

    Last commit didn't include the answers class

---
+++ src/Parser/Answers.java
@@ -0,0 +1,70 @@
+package Parser;
+
+import java.util.ArrayList;
+
+public class Answers {
+
+    private HTMLProcessor processor;
+    private ArrayList<PuzzleWord> answers;
+
+    public Answers(HTMLProcessor processor){
+        this.processor = processor;
+        answers = new ArrayList<PuzzleWord>();
+        initAnswers();
+    }
+    private void initAnswers(){
+        //ACROSS
+        for(int i = 0; i < processor.hints[0].size();i++){
+            String questionNo = processor.hints[0].get(i).substring(0,1);
+            String hint = processor.hints[0].get(i).substring(2,processor.hints[0].get(i).length());
+            int startX = 0;
+            int startY = 0;
+            int size = 0;
+            while(!processor.puzzle[startX][startY].getQuestionNo().equals(questionNo)) {
+                if (startX == 4) {
+                    startX = 0;
+                    startY++;
+                }
+                else
+                    startX++;
+
+            }
+            int tempX = startX;
+            while(tempX < 5){
+                if(processor.puzzle[tempX][startY].getCurrentLetter().equals("-1"))
+                    break;
+                size++;
+                tempX++;
+            }
+            PuzzleWord temp = new PuzzleWord(0,startX,startY,size,Integer.parseInt(questionNo),hint);
+            answers.add(temp);
+            System.out.println("Direction up : " + startX + " ~ " + startY + " Size : "+ size + " Hint: "+hint);
+        }
+        //DOWN
+        for(int i = 0; i < processor.hints[1].size();i++){
+            String questionNo = processor.hints[1].get(i).substring(0,1);
+            String hint = processor.hints[1].get(i).substring(2,processor.hints[1].get(i).length());
+            int startX = 0;
+            int startY = 0;
+            int size = 0;
+            while(!processor.puzzle[startX][startY].getQuestionNo().equals(questionNo)){
+                if(startX == 4){
+                    startX = 0;
+                    startY++;
+                }
+                else
+                    startX++;
+            }
+            int tempY = startY;
+            while(tempY < 5){
+                if(processor.puzzle[startX][tempY].getCurrentLetter().equals("-1"))
+                    break;
+                size++;
+                tempY++;
+            }
+            PuzzleWord temp = new PuzzleWord(1,startX,startY,size,Integer.parseInt(questionNo),hint);
+            answers.add(temp);
+            System.out.println("Direction down : " + startX + " ~ " + startY + " Size : "+ size + " Hint: "+hint);
+        }
+    }
+}
