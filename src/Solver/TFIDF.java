78970ed095483eb6921a97ce50699b0283fa66a3
Doruk Cakmakci <dorukcakmakci97@gmail.com>
2018-04-23 04:29:10 +0300

    Fix PuzzleSolverWithTFIDF

---
+++ src/Solver/TFIDF.java
@@ -0,0 +1,97 @@
+package Solver;
+
+import java.util.ArrayList;
+
+public class TFIDF {
+
+    //DATA MEMBERS
+    private ArrayList<String> google;
+    private ArrayList<String> datamuse;
+    private ArrayList<String> abbreviations;
+    private ArrayList<String> movie;
+    ArrayList<ArrayList<String>> docs;
+
+
+
+    public TFIDF(ArrayList<String> google, ArrayList<String> datamuse, ArrayList<String> movie) {
+        this.google = google;
+        this.datamuse = datamuse;
+        this.movie = movie;
+        docs = new ArrayList<ArrayList<String>>();
+        docs.add(google);
+        docs.add(datamuse);
+        docs.add(movie);
+    }
+
+    public double tf(String term, int type) {
+        double result = 0;
+        for (String word : docs.get(type)) {
+            if (term.equalsIgnoreCase(word))
+                result++;
+
+        }
+        if (docs.get(type).size() == 0)
+            return 0;
+        return result / docs.get(type).size();
+    }
+
+    public double idf(String term) {
+        double n = 0;
+        for (int i = 0; i < docs.size(); i++ ) {
+            for (String word : docs.get(i)) {
+                if (term.equalsIgnoreCase(word)) {
+                    switch(i){
+                        case 0: /*google*/ n++; break;
+                        case 1: /*datamuse*/ n += 2; break;
+                       // case 2: /*abbreviation*/ n += 3; break;
+                        case 2: /*movie*/  n += 3;break;
+                        default: break;
+                    }
+                }
+            }
+        }
+        return Math.log(docs.size() / n);
+    }
+
+    public double tfIdf( String term) {
+
+        double freq = 0;
+        for(int i = 0; i < 3; i++){
+            freq += tf(term, i) * idf(term);
+        }
+        return freq;
+    }
+
+    public ArrayList<String> getGoogle() {
+        return google;
+    }
+
+    public void setGoogle(ArrayList<String> google) {
+        this.google = google;
+    }
+
+    public ArrayList<String> getDatamuse() {
+        return datamuse;
+    }
+
+    public void setDatamuse(ArrayList<String> datamuse) {
+        this.datamuse = datamuse;
+    }
+
+    public ArrayList<String> getAbbreviations() {
+        return abbreviations;
+    }
+
+    public void setAbbreviations(ArrayList<String> abbreviations) {
+        this.abbreviations = abbreviations;
+    }
+
+    public ArrayList<String> getMovie() {
+        return movie;
+    }
+
+    public void setMovie(ArrayList<String> movie) {
+        this.movie = movie;
+    }
+
+}
